0fda9e7b060e3569cdd907056efea95c0fa4c749
Jorge Oliveira <jorgedroliveira10@gmail.com>
2020-09-01 11:55:34 +0100

    added firebase auth and dialogs

--- app/src/main/java/ipp/estg/restaurantfinder/activities/AuthenticationActivity.java
+++ app/src/main/java/ipp/estg/restaurantfinder/activities/AuthenticationActivity.java
@@ -1,5 +1,6 @@
 package ipp.estg.restaurantfinder.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -9,8 +10,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.textfield.TextInputEditText;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 import ipp.estg.restaurantfinder.R;
 
@@ -21,6 +28,7 @@ public class AuthenticationActivity extends AppCompatActivity {
     private TextView forgotPassword;
     private TextInputEditText loginEmail;
     private TextInputEditText loginPassword;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,11 +40,12 @@ public class AuthenticationActivity extends AppCompatActivity {
         loginButton = findViewById(R.id.loginButton);
         signupButton = findViewById(R.id.signupButton);
         forgotPassword = findViewById(R.id.forgotPasswordTextView);
+        this.firebaseAuth = FirebaseAuth.getInstance();
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-
+                signIn(loginEmail.getText().toString(), loginPassword.getText().toString());
             }
         });
 
@@ -70,5 +79,47 @@ public class AuthenticationActivity extends AppCompatActivity {
 
         AlertDialog dialog = alert.create();
         dialog.show();
+    }
+
+    private void signIn(String email, String password) {
+        this.firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()) {
+                            FirebaseUser user = firebaseAuth.getCurrentUser();
+                            Intent nearbyRestaurantsIntent = new Intent(getApplicationContext(), NearbyRestaurants.class);
+                            startActivity(nearbyRestaurantsIntent);
+                        } else {
+                            loginDialog("Login error!", getString(R.string.login_error), getString(R.string.ok_button));
+                        }
+                    }
+                });
+    }
+
+    private void loginDialog(String titleMsg, String textMsg, String textButton) {
+        AlertDialog.Builder alert = new AlertDialog.Builder(this, android.R.style.Theme_Material_Dialog_NoActionBar);
+        alert.setTitle(titleMsg);
+
+        LayoutInflater inflater = getLayoutInflater();
+        View view = inflater.inflate(R.layout.login_dialog, null);
+
+        Button dialogButton = view.findViewById(R.id.loginDialogButton);
+        dialogButton.setText(textButton);
+
+        TextView loginMsg = view.findViewById(R.id.loginMsg);
+        loginMsg.setText(textMsg);
+
+        alert.setView(view);
+
+        AlertDialog dialog = alert.create();
+        dialog.show();
+
+        dialogButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                dialog.dismiss();
+            }
+        });
     }
 }
