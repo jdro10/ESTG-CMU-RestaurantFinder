a0d1936345a716abbd2dbc0c6c23c0a8c7aba898
Jorge Oliveira <jorgedroliveira10@gmail.com>
2020-09-03 15:17:24 +0100

    small changes

--- app/src/main/java/ipp/estg/restaurantfinder/activities/RestaurantSelected.java
+++ app/src/main/java/ipp/estg/restaurantfinder/activities/RestaurantSelected.java
@@ -1,8 +1,13 @@
 package ipp.estg.restaurantfinder.activities;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import ipp.estg.restaurantfinder.R;
 
@@ -12,5 +17,38 @@ public class RestaurantSelected extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_restaurant_selected);
+
+        Toolbar myToolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        onBackPressed();
+        return true;
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+
+        int id = item.getItemId();
+
+        if (id == R.id.action_settings) {
+            Intent intent = new Intent(getApplicationContext(), FavoritesRestaurants.class);
+            startActivity(intent);
+        } else if (id == R.id.action_favourite) {
+            Intent intent = new Intent(getApplicationContext(), FavoritesRestaurants.class);
+            startActivity(intent);
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 }
